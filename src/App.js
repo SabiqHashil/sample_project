78c63ce9514c80837f48d2ea646e2e0095e88694
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-02-24 15:28:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <nav id="nav">
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#news">News</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li class="alogo">
-            <img
-            class="logo"
-              src="https://www.gracevalleycollege.com/images/logo.jpg"
-              alt=""
-            />
-          </li>
-        </ul>
+     <nav id="nav">
+        <div class="navTop">
+          <div class="navItem1">
+            <h1>GVC Student Records</h1>
+            <a class="prefrontpage" href="home.jsx">
+              <i class="fas fa-chevron-left"></i>Home
+            </a>
+            <a class="prefrontpage" href="previous-page.html">
+              <i class="fas fa-chevron-left"></i> Previous Page
+            </a>
+            <a  class="prefrontpage" href="next-page.html">
+              Next Page <i class="fas fa-chevron-right"></i>
+            </a>
+          </div>
+
+          <div class="navItem3">
+           
+          </div>
+
+          <div class="navItem2">
+            <img class="adminicon" src="https://images.shiksha.com/mediadata/images/1591177136phplF67P8.jpeg" alt="" />
+          </div>
+        </div>
       </nav>
     </div>
   );
