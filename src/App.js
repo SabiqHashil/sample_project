90c6df3f0a5c12d86c3d15812e9c15b83f5b363e
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-02-24 16:27:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-     <nav id="nav">
+      <nav id="nav">
         <div class="navTop">
           <div class="navItem1">
             <h1>GVC Student Records</h1>
@@ -14,17 +14,17 @@ function App() {
             <a class="prefrontpage" href="previous-page.html">
               <i class="fas fa-chevron-left"></i> Previous Page
             </a>
-            <a  class="prefrontpage" href="next-page.html">
+            <a class="prefrontpage" href="next-page.html">
               Next Page <i class="fas fa-chevron-right"></i>
             </a>
           </div>
 
-          <div class="navItem3">
-           
-          </div>
-
           <div class="navItem2">
-            <img class="adminicon" src="https://images.shiksha.com/mediadata/images/1591177136phplF67P8.jpeg" alt="" />
+            <img
+              class="adminicon"
+              src="https://images.shiksha.com/mediadata/images/1591177136phplF67P8.jpeg"
+              alt=""
+            />
           </div>
         </div>
       </nav>
